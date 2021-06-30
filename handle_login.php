3b4dbd29d4bd044d69960f469c69254d38fa7f96
duytuan113 <nhanchunggiuadoi@gmail.com>
2021-06-30 23:58:49 +0700

    deploy signin bbcbcbcbcbcbc

--- handle_login.php
+++ handle_login.php
@@ -1,20 +1,23 @@
-<?php 
-   include 'database.php';
 
-         $email = $_POST['email'];
-         $password = $_POST['password'];
-         // SQL query for check user table in mySQL 
-         $sql = "SELECT * FROM admin WHERE email='$email' AND password ='$password' ";
-         // Run sql query
-         $result = pg_query($conn, $sql);
-         $row = pg_fetch_assoc($result); // Tìm cụ thể trên gg
-             
-         if ($row == 0){
-            header('location: sign_in.php');
-         }
-         else{
-            header('location: index.php');
-         }
-         echo ($username) ;
-         echo ($password) ;
+<?php  include('database.php'); ?>
+
+<?php 
+   if (isset($_POST['submit'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        // SQL query for check user table in mySQL 
+        $sql = "SELECT * FROM admin WHERE email='$email' AND password ='$password' ";
+        // Run sql query
+        $result = pg_query($conn, $sql);
+        $row = pg_fetch_assoc($result); // Tìm cụ thể trên gg
+            
+        if ($row == 0){
+        header('location: sign_in.php');
+        }
+        else{
+        header('location: index.php');
+        }
+        echo ($username) ;
+        echo ($password) ;
+   }
 ?>
